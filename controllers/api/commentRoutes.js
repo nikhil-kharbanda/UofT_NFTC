05b75f955ba54a7ffe6d45bc9b0ea0909106e8d9
Nikhil Kharbanda <nikhil_kharbanda@hotmail.com>
2021-10-28 11:47:38 -0400

    added Mikes stuff, removed index. Temp removed controllers, starting from scratch

--- controllers/api/commentRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { Collect, Comment } = require('../../models');
-
-router.get('/', withAuth, async(req,res) => {
-    try {
-        const newComment = await Comment.findAll();
-        
-    }
-})
