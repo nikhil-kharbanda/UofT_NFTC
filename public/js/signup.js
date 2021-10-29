790023fbb4d8f0d024b984f0d992f4f9fd4fb29c
Nikhil Kharbanda <nikhil_kharbanda@hotmail.com>
2021-10-29 15:29:07 -0400

    routing works to login and signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,23 +3,26 @@ async function signupFormHandler(event) {
 
     // getting data from the form
     const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
     if (username && password) {
-        const response = await fetch('/api/users', {
+        const response = await fetch('/api/users/signup', {
           method: 'POST',
           body: JSON.stringify({
-            username,
-            password
+            userName:username,
+            password:password,
+            email:email
           }),
           headers: { 'Content-Type': 'application/json' }
         }); 
     // check the response status
+   
     if (response.ok) {
         console.log('success');
 
         // loginHandler();
-        document.location.replace('/');
+        document.location.replace('/feed');
 
       } else {
         alert(response.statusText);
